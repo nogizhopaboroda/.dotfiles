04459edb9418affaa3e25e03c2fd464c46f9d197
Zhukov <aleksandr.zhukov@sevenonemedia.de>
2018-12-16 17:14:28 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+gem install xcode-install
+
 cd $(mktemp -d) && curl https://raw.githubusercontent.com/nogizhopaboroda/.dotfiles/master/Brewfile.work > Brewfile && brew bundle install
 
 chsh -s $(which zsh)
+
+
+#sudo defaults write com.apple.loginwindow LoginHook `pwd`/remap_keys.sh
