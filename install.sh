fa41ce74086c95c16f355a79e77a9bd6e0bb4aeb
Alexander Zhukov <al.zhukoff@gmail.com>
2018-12-28 19:36:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,49 @@
-gem install xcode-install
+# go home
+cd ~
 
-cd $(mktemp -d) && curl https://raw.githubusercontent.com/nogizhopaboroda/.dotfiles/master/Brewfile.work > Brewfile && brew bundle install
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+#curl brewfile
+#brew bundle install
+# cd $(mktemp -d) && curl https://raw.githubusercontent.com/nogizhopaboroda/.dotfiles/master/Brewfile.default > Brewfile && brew bundle install
+
+# clone repo
+git clone https://github.com/nogizhopaboroda/.dotfiles.git ~/.dotfiles
+
+# make zsh default shell
 chsh -s $(which zsh)
 
+# add custom .zshrc
+cat <<EOT >> ~/.zshrc
+if [ -f ~/.dotfiles/.zshrc ]; then
+  source ~/.dotfiles/.zshrc
+fi
+EOT
+
+# add custom .gitignore
+cat <<EOT >> ~/.gitconfig
+[include]
+    path = ~/.dotfiles/.gitconfig
+EOT
+
+# add custom .vimrc
+cat <<EOT >> ~/.vimrc
+if filereadable(glob("~/.dotfiles/.vimrc"))
+   source ~/.dotfiles/.vimrc
+endif
+EOT
+
+# install neobundle
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
+# gem install xcode-install
+
+# AFTER INSTALL
+
+# install node
+#nvm install node
+
+#vim
 
 #sudo defaults write com.apple.loginwindow LoginHook `pwd`/remap_keys.sh
