1184bf4f29b5746b138680484014695c483aa63a
ni-azhukov <alexander.zhukov@native-instruments.de>
2022-04-28 17:33:34 +0200

    add more lsp servers

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -5,6 +5,9 @@ local lsp_installer = require "nvim-lsp-installer"
 local servers = {
   "tsserver",
   "diagnosticls",
+  "graphql",
+  "jsonls",
+  "sumneko_lua",
 }
 
 for _, name in pairs(servers) do
@@ -16,14 +19,73 @@ end
 
 
 local nvim_lsp = require("lspconfig")
+local opts = { noremap=true, silent=true }
 
-local function on_attach(client, bufnr)
-  -- Set up buffer-local keymaps (vim.api.nvim_buf_set_keymap()), etc.
+vim.api.nvim_set_keymap('n', '<Leader>sd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+
+local on_attach = function(client, bufnr)
+  local buf_map = vim.api.nvim_buf_set_keymap
+  vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+  vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+  vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
+  vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+  vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+  vim.cmd("command! LspOrganize lua lsp_organize_imports()")
+  vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+  vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+  vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+  vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
+  vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
+  -- vim.cmd(
+      -- "command! LspDiagLine lua vim.lsp.diagnostic.open_float()")
+  vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_map(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_map(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_map(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_map(bufnr, 'n', '<Leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_map(bufnr, 'n', '<Leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_map(bufnr, 'n', '<Leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_map(bufnr, 'n', '<Leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_map(bufnr, 'n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_map(bufnr, 'n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_map(bufnr, 'n', '<Leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 local _, tsserver = lsp_installer.get_server('tsserver')
 tsserver:on_ready(function()
-  nvim_lsp.tsserver.setup {}
+  nvim_lsp.tsserver.setup {
+    on_attach = on_attach
+  }
+end)
+
+local _, graphqlserver = lsp_installer.get_server('graphql')
+graphqlserver:on_ready(function()
+  nvim_lsp.graphql.setup {
+    on_attach = on_attach,
+    filetypes = { 'typescript', 'javascript' }
+  }
+end)
+
+local _, jsonlsserver = lsp_installer.get_server('jsonls')
+jsonlsserver:on_ready(function()
+  nvim_lsp.jsonls.setup {
+    on_attach = on_attach
+  }
+end)
+
+local _, luaserver = lsp_installer.get_server('sumneko_lua')
+luaserver:on_ready(function()
+  nvim_lsp.sumneko_lua.setup {
+    on_attach = on_attach
+  }
 end)
 
 local _, diagnosticsserver = lsp_installer.get_server('diagnosticls')
@@ -150,14 +212,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 --          ]], true)
 --     end
 -- end
---
--- nvim_lsp.graphql.setup {
---     cmd = { this_dir .. '/nvim/node_modules/.bin/graphql-lsp', 'server', '-m', 'stream' },
---     filetypes = { 'typescript', 'javascript' }
--- }
---
--- nvim_lsp.jsonls.setup {
---     cmd = { this_dir .. '/nvim/node_modules/.bin/vscode-json-languageserver', '--stdio' },
--- }
---
--- print('plugins config 4')
+
+
+print('plugins config 6')
