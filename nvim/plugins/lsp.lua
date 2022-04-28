0f836424b696da1b70473f878ec21f91d2d65c16
ni-azhukov <alexander.zhukov@native-instruments.de>
2022-04-28 19:59:22 +0200

    remove diagnostics ls

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -4,7 +4,6 @@ local lsp_installer = require "nvim-lsp-installer"
 -- Include the servers you want to have installed by default below
 local servers = {
   "tsserver",
-  "diagnosticls",
   "graphql",
   "jsonls",
   "sumneko_lua",
@@ -57,6 +56,16 @@ local on_attach = function(client, bufnr)
   buf_map(bufnr, 'n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_map(bufnr, 'n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_map(bufnr, 'n', '<Leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+  -- remove thids block if autoformatting is not needed
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_exec([[
+      augroup LspAutocommands
+      autocmd! * <buffer>
+      autocmd BufWritePost <buffer> LspFormatting
+      augroup END
+    ]], true)
+  end
 end
 
 local _, tsserver = lsp_installer.get_server('tsserver')
@@ -88,55 +97,6 @@ luaserver:on_ready(function()
   }
 end)
 
-local _, diagnosticsserver = lsp_installer.get_server('diagnosticls')
-diagnosticsserver:on_ready(function()
-  local filetypes = {
-      json = "eslint",
-      javascript = "eslint",
-      typescript = "eslint",
-      typescriptreact = "eslint",
-  }
-  -- local linters = {
-  --     eslint = {
-  --         sourceName = "eslint",
-  --         command = this_dir .. "/nvim/node_modules/.bin/eslint_d",
-  --         rootPatterns = {".eslintrc.js", "package.json"},
-  --         debounce = 100,
-  --         args = {"--stdin", "--stdin-filename", "%filepath", "--format", "json"},
-  --         parseJson = {
-  --             errorsRoot = "[0].messages",
-  --             line = "line",
-  --             column = "column",
-  --             endLine = "endLine",
-  --             endColumn = "endColumn",
-  --             message = "${message} [${ruleId}]",
-  --             security = "severity"
-  --         },
-  --         securities = {[2] = "error", [1] = "warning"}
-  --     }
-  -- }
-  -- local formatters = {
-  --     prettier = {command = this_dir .. "/nvim/node_modules/.bin/prettier", args = {"--stdin-filepath", "%filepath"}}
-  -- }
-  -- local formatFiletypes = {
-  --     json = "prettier",
-  --     javascript = "prettier",
-  --     typescript = "prettier",
-  --     typescriptreact = "prettier"
-  -- }
-
-  nvim_lsp.diagnosticls.setup {
-      on_attach = on_attach,
-      filetypes = vim.tbl_keys(filetypes),
-      -- init_options = {
-      --     filetypes = filetypes,
-      --     linters = linters,
-      --     formatters = formatters,
-      --     formatFiletypes = formatFiletypes
-      -- }
-  }
-end)
-
 
 
 -- lsp config
@@ -164,6 +124,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 --         end
 --     end
 -- end
+--
 -- vim.lsp.handlers["textDocument/formatting"] = format_async
 -- _G.lsp_organize_imports = function()
 --     local params = {
@@ -173,45 +134,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 --     }
 --     vim.lsp.buf.execute_command(params)
 -- end
--- local on_attach = function(client, bufnr)
---     local buf_map = vim.api.nvim_buf_set_keymap
---     vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
---     vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
---     vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
---     vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
---     vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
---     vim.cmd("command! LspOrganize lua lsp_organize_imports()")
---     vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
---     vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
---     vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
---     vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
---     vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
---     vim.cmd(
---         "command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()")
---     vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
---     buf_map(bufnr, "n", "gd", ":LspDef<CR>", {silent = true})
---     buf_map(bufnr, "n", "<Leader>rn", ":LspRename<CR>", {silent = true})
---     buf_map(bufnr, "n", "gR", ":LspRefs<CR>", {silent = true})
---     buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>", {silent = true})
---     buf_map(bufnr, "n", "K", ":LspHover<CR>", {silent = true})
---     buf_map(bufnr, "n", "gs", ":LspOrganize<CR>", {silent = true})
---     buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>", {silent = true})
---     buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>", {silent = true})
---     buf_map(bufnr, "n", "ga", ":LspCodeAction<CR>", {silent = true})
---     buf_map(bufnr, "n", "<Leader>a", ":LspDiagLine<CR>", {silent = true})
---     buf_map(bufnr, "i", "<C-x><C-x>", "<cmd> LspSignatureHelp<CR>",
---               {silent = true})
---
--- -- remove thids block if autoformatting is not needed
--- if client.resolved_capabilities.document_formatting then
---         vim.api.nvim_exec([[
---          augroup LspAutocommands
---              autocmd! * <buffer>
---              autocmd BufWritePost <buffer> LspFormatting
---          augroup END
---          ]], true)
---     end
--- end
 
 
-print('plugins config 6')
+print('plugins config 7')
